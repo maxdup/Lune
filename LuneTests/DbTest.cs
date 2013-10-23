2552ecede0af009333906b21dff19f45bfd9d8ac
maxdup <mdupuis@hotmail.ca>
2013-10-23 08:10:57 -0400

    added test project to solution
    to test upcoming database code

---
+++ LuneTests/DbTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LuneTests
+{
+    [TestClass]
+    public class DbTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
