aa8ec859f1cc34cf250e2cbd7a02959c1821cee9
maxdup <mdupuis@hotmail.ca>
2014-04-27 01:04:58 -0400

    started basic file search

---
+++ DAL/collector.py
@@ -0,0 +1,17 @@
+import os
+
+
+class Collector():
+
+    FORMATS = ['mp3', 'flac', 'aac', 'ogg']
+
+    def __init__(self):
+        self.library = []
+
+    def searchDir(self, dir):
+        directory = os.walk(dir)
+        for root, dirs, files in directory:
+            for file in files:
+                if file.split('.').pop() in self.FORMATS:
+                    self.library.append(os.path.join(root, file))
+        print((len(self.library)))
