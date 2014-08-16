f438b7c7b317d081f2711846ad5cac75f93516fb
maxdup <mdupuis@hotmail.ca>
2014-08-15 23:56:59 -0400

    using icon font on audio controls

--- views/status/playback_controls.py
+++ views/status/playback_controls.py
@@ -11,28 +11,20 @@ class PlaybackControls(QtGui.QWidget):
         with open('views/qss/playback_controls.qss','r') as stylesheet:
             self.setStyleSheet(stylesheet.read())
 
-        self.previous = QtGui.QPushButton('7')
+        self.previous = QtGui.QPushButton('<')
         self.previous.clicked.connect(player.previous)
-        if platform == "linux" or platform == "linux2" or platform == 'darwin':
-            self.previous.setText('prev')
 
-        self.stop = QtGui.QPushButton('<')
+        self.stop = QtGui.QPushButton('\\')
         self.stop.clicked.connect(player.stop)
         self.stop.setObjectName('stop')
-        if platform == "linux" or platform == "linux2" or platform == 'darwin':
-            self.stop.setText('stop')
 
-        self.pause_resume = QtGui.QPushButton('4')
+        self.pause_resume = QtGui.QPushButton(']')
         self.pause_resume.clicked.connect(player.play_pause)
         self.pause_resume.setObjectName('pauseresume')
 
-        if platform == "linux" or platform == "linux2" or platform == 'darwin':
-            self.pause_resume.setText('play')
 
-        self.skip = QtGui.QPushButton('8')
+        self.skip = QtGui.QPushButton('>')
         self.skip.clicked.connect(player.skip)
-        if platform == "linux" or platform == "linux2" or platform == 'darwin':
-            self.skip.setText('skip')
 
         layout = QtGui.QHBoxLayout(self)
 
