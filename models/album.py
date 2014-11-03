abe2abc5c2580d50116caf40e33f140ee901c59b
maxdup <mdupuis@hotmail.ca>
2014-11-03 02:01:20 -0500

    added artwork for albums

--- models/album.py
+++ models/album.py
@@ -2,8 +2,12 @@ class Album:
     def __init__(self, title='unknown', songs=[]):
         self.title = title
         self.songs = songs
+        self.artwork = None
 
     def get_art(self):
-        if self.songs:
-            #todo: search all indexes adn save the path
-            return self.songs[0].track_info['artwork']
+        if self.songs and not self.artowrk:
+            for song in songs:
+                if song.trackinfo['artwork']:
+                    self.artwork = song.trackinfo['artwork']
+                    break
+        return self.artwork
--- models/song.py
+++ models/song.py
@@ -5,3 +5,6 @@ class Song:
     def __init__(self, path=None, track_info={}):
         self.path = path
         self.track_info = track_info
+        self.album = album
+    def setAlbum(self, album):
+        self.album = album
