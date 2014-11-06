a1dd03363cfde6e0abb364db116d14a307d018ac
maxdup <mdupuis@hotmail.ca>
2014-11-05 19:03:28 -0500

    added folder.jpg as an option for album art

--- models/album.py
+++ models/album.py
@@ -1,3 +1,5 @@
+import os
+
 class Album:
     def __init__(self, title='unknown', songs=[], artist=None):
         self.title = title
@@ -8,7 +10,12 @@ class Album:
     def get_art(self):
         if self.songs and not self.artwork:
             for song in self.songs:
-                if song.track_info['artwork']:
-                    self.artwork = song.track_info['artwork']
+                self.artwork = song.track_info['artwork']  #directly in track info
+                if not self.artwork:
+                    folderjpg = os.path.split(song.path)[0] + '/Folder.jpg'
+                    #multiplatform?
+                    if os.path.exists(folderjpg):
+                        self.artwork = folderjpg
+                if self.artwork:
                     break
         return self.artwork
