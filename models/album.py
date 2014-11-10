633d54520f0e65d62645c94eab05cc74997217b6
maxdup <mdupuis@hotmail.ca>
2014-11-10 08:20:50 -0500

    finished lazy loading/unloading

--- models/album.py
+++ models/album.py
@@ -9,17 +9,18 @@ class Album:
         self.placeholder = None
 
     def get_art(self):
+        if not self.songs:
+            return
         if not self.artwork or not os.path.exists(self.artwork):
-
-            for song in self.songs:
-                if song.track_info['artwork'] and os.path.exists(
-                        song.track_info['artwork']):
-                    self.artwork = song.track_info['artwork']
-                    break
-            if not self.artwork:
-                filenames = ['/Folder.jpg', '/folder.jpg']
-                for name in filenames:
-                    path = os.path.split(song.path)[0] + name
-                    if os.path.exists(path):
-                        self.artwork = path
+            filenames = ['/Folder.jpg', '/folder.jpg']
+            for name in filenames:
+                path = os.path.split(self.songs[0].path)[0] + name
+                if os.path.exists(path):
+                    self.artwork = path
+                if not self.artwork:
+                    for song in self.songs:
+                        if song.track_info['artwork'] and os.path.exists(
+                                song.track_info['artwork']):
+                            self.artwork = song.track_info['artwork']
+                            break
         return self.artwork
